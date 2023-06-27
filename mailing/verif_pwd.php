8235e25b06f49dc2c00aa9d728cbed2e129f7256
Redak92 <bastienguibert98@gmail.com>
2023-06-27 19:04:50 +0100

    Password reset

--- mailing/verif_pwd.php
+++ mailing/verif_pwd.php
@@ -23,40 +23,33 @@ if(isset($_GET['key'])){
     $requser-> execute([$key]);
 
     $userexists = $requser->rowCount();
+	
 // UPDATE de la table SI l'utilisateur existe
 
-    if($userexists == 1){
-        $user = $requser->fetch();
-        if($user['is_valid'] == 0){
-            $update_user = $bdd->prepare("UPDATE users SET is_valid = 1 WHERE confirm_key = ?");
-            $update_user -> execute([$key]);
-            echo "Votre compte a bien été confirmé ! ";
-        }else{
-            echo "Cet utilisateur a déjà été vérifié";
-        }
-
-    }else {
-        header("location: index.php");
-    }
+    if($userexists != 1){
+		header("location: ../index.php");
+		exit;
+	}
 	
-//UPDATE de la key
-$q_id = $bdd->prepare("SELECT id FROM users WHERE email=?");
-$q_id->execute([$user['email']]);
-$data = $q_id->fetchAll();
+$title = "Réinitialiser mot de passe";
+include("../includes/head.php");
 
-
-
-$key = "";
-	for($i=1;$i < 12;$i++){
-
-    $key .= mt_rand(0,9);
-
-  
+}else{
+	header("location: ../index.php");
 }
+ ?>
+ 
+ <main>
+	
+	<form action = "update_pwd.php" method = "POST">
+		<input type = "password" name = "pwd" placeholder = "Votre mot de passe">
+		<input type = "password" name = "pwd_verif" placeholder = "Confirmer mot de passe">
+		<input type = "hidden" name = "key" value = "<?= $key; ?>">
+		<input type = "submit">
+		
+	
+	</form>
+ 
+ 
+ </main>
 
-$key .= $data[0]['id'];
-
-$last = $bdd -> prepare("UPDATE users SET confirm_key=? WHERE email=?");
-$last->execute([$key,$user['email']]);
-
-}
